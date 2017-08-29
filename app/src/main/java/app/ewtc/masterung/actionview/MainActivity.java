829f97691b2a10fdee947e476dcc612317314325
MasterUNG <phrombutr@gmail.com>
2017-08-29 12:34:58 +0700

    Project Version1 Success

--- app/src/main/java/app/ewtc/masterung/actionview/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/actionview/MainActivity.java
@@ -5,26 +5,61 @@ import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private String urlString = "http://www.inspectorrta.org/IGD2017/";
+    private int anInt = 0;
+    private ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            myIntentToActionView();
+        }
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setProgress(0);
+
+        myLoop();
+
+    }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    private void myDelay() {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                myIntentToActionView();
+                myLoop();
             }
-        }, 3000);
+        }, 200);
+    }
 
+    private void myLoop() {
+        //Todo
+        //Increase Integer
+        anInt += 5;
 
-    }   // Main Method
+        //Check Integer
+        if (anInt > 100) {
+            myIntentToActionView();
+        }
+
+        progressBar.setProgress(anInt);
+
+        myDelay();
+    }
 
     private void myIntentToActionView() {
         Intent intent = new Intent(Intent.ACTION_VIEW);
